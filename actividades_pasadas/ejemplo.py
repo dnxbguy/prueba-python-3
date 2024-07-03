9b04ef5747066caaf4f7a72f0739816bb941e25c
dnxbguy <ben.perezb@duocuc.cl>
2024-07-03 00:14:09 -0400

    primer commit

---
+++ actividades_pasadas/ejemplo.py
@@ -0,0 +1,10 @@
+lista=[]
+print("dame 3 nombres")
+for i in range(0,3):
+    ele = str(input()) 
+    
+    lista.append(ele)
+    
+print(lista)
+for i in lista
+print(caracter)
---
+++ actividades_pasadas/ola.py
@@ -0,0 +1,13 @@
+# creating an empty list
+lst = []
+
+# number of elements as input
+(print("dame 3 nombres"))
+# iterating till the range
+for i in range(0, 3):
+	ele = str(input())
+	# adding the element
+	lst.append(ele) 
+
+
+print(lst)
